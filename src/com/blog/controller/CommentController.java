5c88183ba0e5b86fa8f2e9951194ebb11a937bf4
瞿龙俊 <154256698@qq.com>
2017-07-17 17:26:02 +0800

    评论模块完成

--- src/com/blog/controller/CommentController.java
+++ src/com/blog/controller/CommentController.java
@@ -5,9 +5,14 @@ import com.blog.domain.Comment;
 import com.blog.utils.ParaUtils;
 import com.blog.utils.RenderUtils;
 import com.jfinal.core.Controller;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 import com.jfinal.render.Render;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by qulongjun on 2017/7/16.
@@ -28,12 +33,73 @@ public class CommentController extends Controller {
                         .set("content", getPara("content"))
                         .set("article_id", getPara("id"))
                         .set("state", 1)
-                        .set("parent", getPara("parent"));
+                        .set("parent", getParaToInt("parent"))
+                        .set("photo", getParaToInt("photo"));
                 Boolean result = comment.save();
-                renderJson(result ? RenderUtils.CODE_SUCCESS : RenderUtils.CODE_ERROR);
+                Map temp = RenderUtils.codeFactory(200);
+                temp.put("comment", comment.get("id"));
+                renderJson(result ? temp : RenderUtils.CODE_ERROR);
             }
         } catch (Exception e) {
             renderError(500);
         }
+    }
+
+
+    /**
+     * 获取评论列表
+     */
+    public void list() {
+        Integer id = getParaToInt("id");
+        List<Record> recordList = Db.find("SELECT c.* FROM `db_comment` c WHERE c.parent IS NULL AND c.article_id =" + id);
+        String json = "";
+        for (int i = 0; i < recordList.size(); i++) {
+            Record record = recordList.get(i);
+            if (record != null) {
+                json += findChild(record, id) + ",";
+            }
+        }
+        if (recordList.size() > 0) {
+            json = json.substring(0, json.length() - 1);
+        }
+        renderJson("{\"results\":[" + json + "]}");
+    }
+
+
+    public String findChild(Record parent, int blog_id) {
+        List<Record> recordList = Db.find("SELECT c.* FROM `db_comment` c WHERE c.parent =" + parent.get("id") + " AND c.article_id =" + blog_id);
+        String json = "";
+        if (recordList.size() == 0) {
+            //当前已经是最底层了,迭代结束,拼接JSON
+            json = "{" +
+                    "\"id\":" + parent.get("id") + "," +
+                    "\"name\":\"" + parent.get("name") + "\"," +
+                    "\"parent\":" + parent.get("parent") + "," +
+                    "\"photo\":" + parent.get("photo") + "," +
+                    "\"create_time\":\"" + parent.get("create_time") + "\"," +
+                    "\"content\":\"" + parent.get("content") + "\"" +
+                    "}";
+            return json;
+        } else {
+            //往下层迭代
+            String commentJSON = "";
+            for (int i = 0; i < recordList.size(); i++) {
+                Record comment = recordList.get(i);
+                if (comment != null) {
+                    commentJSON += findChild(comment, blog_id) + ",";
+                }
+            }
+            commentJSON = commentJSON.substring(0, commentJSON.length() - 1);
+            json = "{" +
+                    "\"id\":" + parent.get("id") + "," +
+                    "\"name\":\"" + parent.get("name") + "\"," +
+                    "\"children\":[" + commentJSON + "]," +
+                    "\"parent\":" + parent.get("parent") + "," +
+                    "\"photo\":" + parent.get("photo") + "," +
+                    "\"create_time\":\"" + parent.get("create_time") + "\"," +
+                    "\"content\":\"" + parent.get("content") + "\"" +
+                    "}";
+        }
+        return json;
     }
 }
