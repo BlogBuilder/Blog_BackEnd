1c1da425bbbb58ef5cb5360b23308139ee2de013
瞿龙俊 <154256698@qq.com>
2017-07-21 11:22:07 +0800

    标签管理完成

--- src/com/blog/controller/TagController.java
+++ src/com/blog/controller/TagController.java
@@ -1,9 +1,14 @@
 package com.blog.controller;
 
 import com.blog.domain.Tag;
+import com.blog.utils.ParaUtils;
 import com.blog.utils.RenderUtils;
 import com.jfinal.core.Controller;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.IAtom;
+import com.jfinal.plugin.activerecord.Page;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,6 +39,116 @@ public class TagController extends Controller {
             Map result = new HashMap();
             result.put("results", tagList);
             renderJson(result);
+        } catch (Exception e) {
+            renderError(500);
+        }
+    }
+
+    public void adminList() {
+        try {
+            int rowCount = getParaToInt("rowCount");
+            int currentPage = getParaToInt("currentPage");
+            String condition_temp = getPara("condition");
+            Map condition = ParaUtils.getSplitCondition(condition_temp);
+            if (rowCount == 0) {
+                rowCount = ParaUtils.getRowCount();
+            }
+            String paras = "WHERE 1=1 ";
+            String tables = " FROM `db_tag` t ";
+            Object[] keys = condition.keySet().toArray();
+            boolean flag = false;
+            for (int i = 0; i < keys.length; i++) {
+                String key = (String) keys[i];
+                Object value = condition.get(key);
+                switch (key) {
+                    case "key":
+                        paras += " AND t.name like \"%" + value + "%\" ";
+                        break;
+                    case "state":
+                        flag = true;
+                        paras += " AND t.state=" + value + " ";
+                        break;
+                }
+            }
+            if (!flag) {
+                paras += " AND t.state=1 ";
+            }
+
+            Page<Tag> tagPage = Tag.tagDao.paginate(currentPage, rowCount, "SELECT t.*", tables + paras);
+            List<Tag> tagList = tagPage.getList();
+            Map results = new HashMap();
+            results.put("results", Tag._toListJson(tagList));
+            results.put("currentPage", currentPage);
+            results.put("totalPage", tagPage.getTotalPage());
+            results.put("rowCount", rowCount);
+            results.put("condition", condition_temp);
+            renderJson(results);
+        } catch (Exception e) {
+            renderError(500);
+        }
+    }
+
+
+    /**
+     * 查找文章标签
+     */
+    public void findById() {
+        try {
+            int id = getParaToInt("id");
+            Tag tag = Tag.tagDao.findFirst("SELECT * FROM `db_tag` WHERE state=1 AND id=" + id);
+            renderJson(tag);
+        } catch (Exception e) {
+            renderError(500);
+        }
+    }
+
+    /**
+     * 修改文章标签
+     */
+    public void change() {
+        try {
+            int id = getParaToInt("id");
+            Tag tag = Tag.tagDao.findById(id);
+            if (tag != null) {
+                tag.set("name", getPara("name"));
+                renderJson(tag.update() ? RenderUtils.CODE_SUCCESS : RenderUtils.CODE_ERROR);
+            } else renderJson(RenderUtils.CODE_EMPTY);
+        } catch (Exception e) {
+            renderError(500);
+        }
+    }
+
+    /**
+     * 删除文章分类
+     */
+    public void delete() {
+        try {
+            int id = getParaToInt("id");
+            Boolean result = Tag.tagDao.deleteById(id);
+            renderJson(result ? RenderUtils.CODE_SUCCESS : RenderUtils.CODE_ERROR);
+        } catch (Exception e) {
+            renderError(500);
+        }
+    }
+
+
+    /**
+     * 批量删除
+     */
+    public void deleteAll() {
+        try {
+            Boolean result = Db.tx(new IAtom() {
+                @Override
+                public boolean run() throws SQLException {
+                    Boolean result = true;
+                    Integer[] ids = getParaValuesToInt("selected[]");
+                    for (int id : ids) {
+                        result = result && Tag.tagDao.deleteById(id);
+                    }
+                    return result;
+                }
+            });
+            renderJson(result ? RenderUtils.CODE_SUCCESS : RenderUtils.CODE_ERROR);
         } catch (Exception e) {
             renderError(500);
         }
